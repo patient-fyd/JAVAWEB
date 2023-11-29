c84e28aee50d429360db1a6276035f3634516e1d
GanFengYu <patient.fyd@gmail.com>
2023-11-29 11:13:33 +0800

    JSON和map、list转换

--- workspaceidea/demo-json/src/com/fyd/test/TestJson.java
+++ workspaceidea/demo-json/src/com/fyd/test/TestJson.java
@@ -7,6 +7,11 @@ import com.fyd.pojo.Dog;
 import com.fyd.pojo.Person;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class TestJson {
 
 
@@ -30,4 +35,35 @@ public class TestJson {
         Person person = objectMapper.readValue(personStr, Person.class);
         System.out.println(person);
     }
+
+    @Test
+    public void testMapToJson() throws JsonProcessingException {
+        Map data = new HashMap();
+        data.put("a","valuea");
+        data.put("b","valueb");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String s = objectMapper.writeValueAsString(data);
+        System.out.println(s);
+    }
+
+    @Test
+    public void testListToJson() throws JsonProcessingException {
+//        List data = new ArrayList();
+//        data.add("a");
+//        data.add("b");
+//        data.add("c");
+
+//        String [] data = {"a","b","c"};
+
+        Dog dog = new Dog("小黄");
+        Person person = new Person("张三",10,dog);
+        List data = new ArrayList();
+        data.add(person);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String s = objectMapper.writeValueAsString(data);
+        System.out.println(s);
+    }
+
 }
