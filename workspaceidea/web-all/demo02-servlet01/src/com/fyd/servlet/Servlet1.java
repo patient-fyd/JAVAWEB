3715c6141b1822c9d8c4df0126df56f5b44e51b8
GanFengYu <patient.fyd@gmail.com>
2023-12-11 14:26:15 +0800

    注解

--- workspaceidea/web-all/demo02-servlet01/src/com/fyd/servlet/Servlet1.java
+++ workspaceidea/web-all/demo02-servlet01/src/com/fyd/servlet/Servlet1.java
@@ -1,12 +1,14 @@
 package com.fyd.servlet;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
+@WebServlet(name = "servlet1", value = {"/s1","/servlet1"})
 public class Servlet1 extends HttpServlet {
 
     @Override
